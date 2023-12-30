5f6cacf7a6cdc4c600e4b0297784a3923b0669ee
John Montgomery <john.montgomery2012@gmail.com>
2023-12-30 18:01:46 -0500

    add markdown preview/test

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,21 +4,7 @@
 -- See the kickstart.nvim README for more information
 --
 --
+require("custom.plugins.markdown_preview")
+require("custom.plugins.harpoon")
 return {
-        "ThePrimeagen/harpoon",
-	branch = "harpoon2",
-	config = function()
-		local harpoon = require("harpoon")
-		---@diagnostic disable-next-line: missing-parameter
-		harpoon:setup()
-		local function map(lhs, rhs, opts)
-                vim.keymap.set("n", lhs, rhs, opts or {})
-		end
-		map("<leader>t", function() harpoon:list():append() end)
-		map("<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-		map("<c-h><c-h>", function() harpoon:list():select(1) end)
-		map("<c-h><c-j>", function() harpoon:list():select(2) end)
-		map("<c-h><c-k>", function() harpoon:list():select(3) end)
-		map("<c-h><c-l>", function() harpoon:list():select(4) end)
-	end
 }
