15935265d3edcc9827bc9e7779805fd607e35f29
John Montgomery <john.montgomery2012@gmail.com>
2024-04-26 09:11:53 -0400

    harpoon working i swear

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,22 +1,25 @@
-local harpoon -- Ensure we only interact with local harpoon object 🦈
-
 return {
   'ThePrimeagen/harpoon',
-  branch = 'harpoon2', -- Temp; Will be merged soonish 🔥
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-  },
-  config = function ()
-    harpoon = require("harpoon"):setup() -- Notice we're storing to parent context 👆
-    local ui = harpoon.ui
+  config = function()
+    require("telescope").load_extension('harpoon')
+    require("harpoon").setup{
+      -- set marks specific to each git branch inside git repository
+      mark_branch = false,
+    }
+    local mark = require("harpoon.mark")
+    vim.keymap.set('n', '<leader>ha', mark.add_file)
+    vim.keymap.set('n', '<leader>hr', mark.rm_file)
 
-    vim.keymap.set("n", "<leader>ht", function() harpoon:list():append() end)
-    vim.keymap.set("n", "<leader>he", function() harpoon:list():remove() end)
-    vim.keymap.set("n", "<leader>hm", function() ui:toggle_quick_menu(harpoon:list()) end)
+    local ui = require("harpoon.ui")
+    vim.keymap.set('n', '<leader>hn', ui.nav_next)
+    vim.keymap.set('n', '<leader>hp', ui.nav_prev)
+    vim.keymap.set('n', '<leader>hm', ui.toggle_quick_menu)
+    vim.keymap.set('n', '<leader>tm', require("telescope").extensions.harpoon.marks)
 
-    vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
-  end,
+      -- C-1, C-2, C-3, C-4 work in nvim/alacritty, but not when inside tmux
+      -- still looking into this functionality, see extended keys
+    vim.keymap.set("n", "<C-1>", function() ui.nav_file(1) end)
+    vim.keymap.set("n", "<C-2>", function() ui.nav_file(2) end)
+    vim.keymap.set("n", "<C-3>", function() ui.nav_file(3) end)
+  end
 }
